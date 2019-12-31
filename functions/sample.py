f0b45bedd2384c68a5b126779dcd80b9b2090bd4
shuntaka9576 <shuntaka9576@gmail.com>
2019-12-31 21:32:49 +0900

    Add typing sample

--- functions/sample.py
+++ functions/sample.py
@@ -1,14 +1,30 @@
 import datetime
 import time
+import typing
 
 import matplotlib.pyplot as plt
+
+
+class Person:
+    def __init__(self, name: str, age: int):
+        self.name: str = name
+        self.age: int = age
+
 
 if __name__ == "__main__":
     ut = time.time()
     dt_now = datetime.datetime.now()
     print("unix:%s, datetime:%s".format(ut, dt_now))
 
+    year: typing.List[int] = []
     year = [1980, 1985, 1990, 2000, 2010, 2018]
+
     weight = [3, 15, 25, 55, 62, 58]
     plt.plot(year, weight)
     plt.show()
+
+    year.append('aaa')
+    persons = Person("takahahsi", 14)
+    persons = Person(13, "takahahsi")
+
+
